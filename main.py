948c2b417297216b15a8d1fa2d4c3a76a255ebfb
Giciarz <106958882+Giciarz@users.noreply.github.com>
2022-06-12 14:00:21 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from methods import Machine
 
+
 def main():
 
-    Machine.set_calibration(123, "chyba tak")
+    big_machine = Machine(None, None, None, None, None, None)
+    big_machine.frequency = int(input("Frequency: \n"))
+    big_machine.power = int(input("Power: \n"))
+    big_machine.voltage = int(input("Voltage: \n"))
+    # big_machine.calibration ?!
+    big_machine.number_of_turns = int(input("Number of turns: \n"))
+    big_machine.type_of_power = str(input("Type of power: "))
+
+    print(f"""
+    Frequency: {big_machine.frequency}
+    Power: {big_machine.power}
+    Voltage: {big_machine.voltage}
+    Number of turns: {big_machine.number_of_turns}
+    Type of power: {big_machine.type_of_power}
+    """)
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
